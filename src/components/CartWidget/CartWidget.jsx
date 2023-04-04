8898fbf3e4a46d063463dc0ea9e5c8faf316cd02
Enzo Scodellaro <scodellaroenzo15@gmail.com>
2023-04-04 11:35:55 -0300

    PreEntrega1+Scodellaro

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,11 @@
 import { GiShoppingCart } from "react-icons/gi";
+import styles from "./CartWidget.module.css";
 
 const CartWidget = () => {
   return (
-    <div>
-      <GiShoppingCart size={20} />
+    <div className={styles.cart}>
+      <GiShoppingCart className={styles.cartMenuIcon} size={20} />
+      <span className={styles.cartMenuNum}>0</span>
     </div>
   );
 };
